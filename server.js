cbe5bac24cc7375c2e97828d1b8e550cbb497f29
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-01 23:39:05 -0500

    Get route to get all saved messages

--- server.js
+++ server.js
@@ -2,10 +2,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from 'pusher'
 
 // app config
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
+
+const pusher = new Pusher({
+  appId: "1100621",
+  key: "8ab09b5f25f4daadf04e",
+  secret: "4d504f3faa3df0facccd",
+  cluster: "us2",
+  useTLS: true
+});
 
 // middleware
 app.use(express.json());
@@ -20,22 +29,20 @@ mongoose.connect(connection_url, {
   useUnifiedTopology: true,
 });
 
-//
-
 // api routes
-app.get("/", (req, res) => res.status(200).send("dean"));
+app.get("/", (req, res) => res.status(200).send("Hello World"));
 
-app.get('./messages/sync'), (req, res) => {
-  Messages.find(err, data) => {
-    if(err) {
-      res.status(500).send(err)
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.send(500).send(err);
     } else {
-      res.status(200).send(data)
+      res.send(200).send(data);
     }
-  }
-}
+  });
+});
 
-app.post("./messages/new", (req, res) => {
+app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
 
   Messages.create(dbMessage, (err, data) => {
